2b3342f47b35697907bc956fe9f2c6e44a9811d3
junhe <774866212@qq.com>
2024-09-19 17:34:55 -0400

    update my folder's structure

---
+++ pythoninfo.py
@@ -0,0 +1,22 @@
+print("hello world");
+var_list = []
+var_list.append("program")
+print(var_list.count("r"))
+
+
+var_dic = {}
+var_dic[111] = "dsfkjdsk"
+var_dic[222] = ["is","dfd"]
+print(var_dic)
+print(var_dic.keys,var_dic.values)
+
+a={1,2,3}
+b=a
+b.remove(3)
+print(a)
+
+aa = 33
+ba = 200
+if ba > aa:
+  print("b is greater than a")
+
